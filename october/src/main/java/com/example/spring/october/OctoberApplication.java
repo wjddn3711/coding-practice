90c7bb548f73498d3314245db35bcbe3ddaeaddf
JungWooLee <50311545+wjddn3711@users.noreply.github.com>
2021-10-31 19:48:40 +0900

    Add files via upload

---
+++ october/src/main/java/com/example/spring/october/OctoberApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.october;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoberApplication.class, args);
+	}
+
+}
---
+++ october/src/test/java/com/example/spring/october/OctoberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.october;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctoberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
